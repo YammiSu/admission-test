d9fca7058f2b4cdd6aefe08e811dcd2e6d819582
YammiSu <candytree2001@gmail.com>
2023-03-19 23:02:55 +0800

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# admission-test
- 
--- question2.py
+++ question2.py
@@ -1,7 +1,23 @@
-count(arr):
+def count(input1):
     count_dict = {}
-    for char in arr:
-        count_dict[char] = count_dict.get(char, 0) 
+    for char in input1:
+        count_dict[char] = count_dict.get(char, 0) + 1 
     return count_dict
-arr= ['a', 'b', 'c', 'a', 'c', 'a', 'x'] 
-print (count(arr))
+input1= ['a', 'b', 'c', 'a', 'c', 'a', 'x'] 
+print (count(input1))
+
+
+def group_by_key(input2):
+    group_dict = {}
+    for dic in input2:
+        group_dict[dic["key"]] = group_dict.get(dic["key"], 0) + dic["value"]
+    return group_dict
+
+input2 = [ 
+{'key': 'a', 'value': 3}, 
+{'key': 'b', 'value': 1}, 
+{'key': 'c', 'value': 2}, 
+{'key': 'a', 'value': 3}, 
+{'key': 'c', 'value': 5} 
+] 
+print(group_by_key(input2))
